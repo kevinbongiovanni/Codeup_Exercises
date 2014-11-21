984af47f70c4cb0157929f27003ea25dd667fd10
Kevin Bongiovanni <whoppingbark@gmail.com>
2014-11-21 13:45:22 -0600

    do while exercise

---
+++ do_while.php
@@ -0,0 +1,22 @@
+<?php
+
+// $a = 0;
+// do {
+//     echo "\$a is equal to {$a}\n";
+//     $a += 2;
+// } while ($a <= 100);
+
+
+
+// $a = 100;
+// do {
+//     echo "\$a is equal to {$a}\n";
+//     $a -= 5;
+// } while ($a >= -10);
+
+$a = 2;
+
+do {
+    echo "\$a is equal to {$a}\n";
+    $a *= 2;
+} while ($a < 1000000);
